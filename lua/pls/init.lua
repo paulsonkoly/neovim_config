8378cb9479694c9cc16e66969204e28f161ff69e
Paul Sonkoly <sonkoly.pal@gmail.com>
2023-10-20 19:11:44 +0000

    new neovim config

---
+++ lua/pls/init.lua
@@ -0,0 +1,9 @@
+require("pls.remap")
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.colorcolumn = "80"
+vim.opt.hlsearch = false
+vim.opt.hidden = true
---
+++ lua/pls/remap.lua
@@ -0,0 +1,6 @@
+vim.g.mapleader = "#"
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<C-h>", "<C-w>h", { remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { remap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { remap = true })
