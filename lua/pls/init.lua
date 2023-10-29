d6482bac37af0c2555808d3d36f53c6c35ad606a
Paul Sonkoly <sonkoly.pal@gmail.com>
2023-10-29 11:59:41 +0000

    disable anoying inline diagnostics

--- lua/pls/init.lua
+++ lua/pls/init.lua
@@ -10,3 +10,11 @@ vim.opt.hlsearch = false
 vim.opt.hidden = true
 vim.opt.showmode = false
 vim.opt.laststatus = 3
+
+-- disable inline diagnostics text
+-- maps working: gl, [d, ]d
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
