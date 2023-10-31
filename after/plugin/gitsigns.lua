42f76c6595e729a3d1202e358ac9a1f95f119184
Paul Sonkoly <paul.sonkoly@smartpension.co.uk>
2023-10-31 08:50:21 +0000

    added mappings for blame and hunk undo

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -1,5 +1,9 @@
-gitsigns = require('gitsigns').setup {
+local gitsigns = require('gitsigns')
+
+gitsigns = gitsigns.setup {
   on_attach = function(bufnr)
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hp', '<cmd>lua require"gitsigns".preview_hunk_inline()<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>hu', '<cmd>lua require"gitsigns".reset_hunk()<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>w', '<cmd>lua require"gitsigns".toggle_current_line_blame()<CR>', {})
   end
 }
