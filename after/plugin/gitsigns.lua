a64cc8255fdaf96bb4c8444fd7b35a7292b16409
Paul Sonkoly <sonkoly.pal@gmail.com>
2024-01-21 12:44:36 +0000

    jump on hunks

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -7,5 +7,7 @@ gitsigns = gitsigns.setup {
     vim.keymap.set('n', '<leader>hp', gs.preview_hunk_inline, { buffer = bufnr })
     vim.keymap.set('n', '<leader>hu', gs.reset_hunk, { buffer = bufnr })
     vim.keymap.set('n', '<leader>w', gs.toggle_current_line_blame, { buffer = bufnr })
+    vim.keymap.set('n', '[c', gs.prev_hunk, { buffer = bufnr })
+    vim.keymap.set('n', ']c', gs.next_hunk, { buffer = bufnr })
   end
 }
