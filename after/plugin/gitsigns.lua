bcce54e499542f1ea2af9d53f7dd954b835d3998
Paul Sonkoly <sonkoly.pal@gmail.com>
2024-03-04 15:28:21 +0000

    added key combo for hunk staging

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -6,6 +6,7 @@ gitsigns = gitsigns.setup {
 
     vim.keymap.set('n', '<leader>hp', gs.preview_hunk_inline, { buffer = bufnr })
     vim.keymap.set('n', '<leader>hu', gs.reset_hunk, { buffer = bufnr })
+    vim.keymap.set('n', '<leader>hs', gs.stage_hunk, { buffer = bufnr })
     vim.keymap.set('n', '<leader>w', gs.toggle_current_line_blame, { buffer = bufnr })
     vim.keymap.set('n', '[c', gs.prev_hunk, { buffer = bufnr })
     vim.keymap.set('n', ']c', gs.next_hunk, { buffer = bufnr })
