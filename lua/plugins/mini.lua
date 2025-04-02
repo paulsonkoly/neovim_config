41a725c59c0d4a609212d2ad5864e9bbce935759
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-04-02 17:45:49 +0100

    removed mini.icons. Added wrapper for mini.notification

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,7 +1,12 @@
 return {
   'echasnovski/mini.ai',
   'echasnovski/mini.surround',
-  { 'echasnovski/mini.notify', config = true },
-  'echasnovski/mini.icons',
+  {
+    'echasnovski/mini.notify',
+    config = function(lazy)
+      local mn = require(lazy.name)
+      vim.notify = mn.make_notify()
+    end
+  },
   'echasnovski/mini.align',
 }
