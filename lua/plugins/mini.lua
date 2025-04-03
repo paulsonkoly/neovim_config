7776bb82622abe3cd35c7dd628cc0504d8703a91
Paul Sonkoly <sonkoly.pal@gmail.com>
2025-04-03 09:57:24 +0100

    rounded mini.notify borders

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,12 +1,19 @@
 return {
-  { 'echasnovski/mini.ai', config = true},
-  { 'echasnovski/mini.surround', config = true},
+  { 'echasnovski/mini.ai',       config = true },
+  { 'echasnovski/mini.surround', config = true },
   {
     'echasnovski/mini.notify',
     config = function(lazy)
       local mn = require(lazy.name)
+      mn.setup({
+        window = {
+          config = {
+            border = "rounded"
+          },
+        },
+      })
       vim.notify = mn.make_notify()
     end
   },
-  { 'echasnovski/mini.align', config = true},
+  { 'echasnovski/mini.align', config = true },
 }
